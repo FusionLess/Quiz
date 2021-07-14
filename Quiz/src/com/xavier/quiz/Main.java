81827296704f047368b8977026fa2d9a6f71b35d
FusionLess <78126285+FusionLess@users.noreply.github.com>
2021-07-14 16:19:57 +1200

    First iteration of the quiz game

---
+++ Quiz/src/com/xavier/quiz/Main.java
@@ -0,0 +1,9 @@
+package com.xavier.quiz;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Quiz quiz = new Quiz(); //allows the use and recognition of the "Quiz.class" file within the main class
+	}
+}
---
+++ Quiz/src/com/xavier/quiz/Quiz.java
@@ -0,0 +1,27 @@
+package com.xavier.quiz;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Quiz implements ActionListener {
+	
+	public Quiz() { //creates the Quiz constructor (a fancy way of saying a method that will always run when a new objectg is created)
+		
+	}
+	public void nextQuestion() { //emthod for generating the next question
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void displayCorrectResult() { //method for displaying the correct result when the answer is either incorrect or correct
+		
+	}
+	public void results() { //method for diplaying the results at the end of the quiz
+		
+	}
+
+}
