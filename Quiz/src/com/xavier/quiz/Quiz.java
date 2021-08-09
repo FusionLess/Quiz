147e219c3ef40af114389857a5bfb3ba99eb7040
FusionLess <78126285+FusionLess@users.noreply.github.com>
2021-08-09 15:58:52 +1200

    Final version for moderation

--- Quiz/src/com/xavier/quiz/Quiz.java
+++ Quiz/src/com/xavier/quiz/Quiz.java
@@ -3,7 +3,6 @@ package com.xavier.quiz;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
-
 public class Quiz implements ActionListener {
 	
 	String[] questions = {
@@ -22,7 +21,7 @@ public class Quiz implements ActionListener {
 							"In programming, the ternary operator is mostly defined with what symbol(s)?",
 							"Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
 							"Generally, which component of a computer draws the most power?"
-						 }; 
+						 }; // creates array of questions
 	
 	String[][] options = {
 							{"eBay", "Overstock", "Shopify", "Amazon"},
@@ -33,14 +32,14 @@ public class Quiz implements ActionListener {
 							{"Gaming Processor Unit","Graphite Producing Unit", "Graphics Processing Unit", "Graphical Proprietary Unit"},
 							{"Eclair", "Honeycomb", "Froyo","Marshmellow"},
 							{"104", "56", "69", "114"},
-							{"Midnight, July 4, 1976","Midnight on the creator of Unix's birthday","Midnight, July 4, 1980", "Midnight, January 1, 1970"},
+							{"Midnight, September 11, 2001","Midnight, April 20, 1969","Midnight, July 4, 1980", "Midnight, January 1, 1970"},
 							{"Howard Packmann", "Hewlett-Packard", "Husker-Pollosk","Hellman-Pohl"},
 							{"CPU Vent","Temperature Decipator","Heat Vent", "Heat Sink"},
 							{"print(\"hello world\")", "console.log\"Hello World\")","echo \"Hello World\"","printf(\"Hello World\")"},
 							{"??","if then","!", "?"},
 							{"PHP", "C#", "JavaScript", "Python"},
 							{"CPU", "Hard drive", "Power Supply", "Graphics Card"}
-						 }; 
+						 }; // creates array of selectable answers
 	
 	char[] answers = 	 {
 							'D',
@@ -58,7 +57,8 @@ public class Quiz implements ActionListener {
 							'D',
 							'C',
 							'C',
-						 }; 
+						 }; // creates the array of coorect answers
+
 	
 	char guess; 
 	char answer;
@@ -85,8 +85,9 @@ public class Quiz implements ActionListener {
 	JLabel seconds_left = new JLabel(); 
 	JTextField number_right = new JTextField(); 
 	JTextField percentage = new JTextField();
+	// sets up variables for the buttons, title, questions, the seconds left and percentage displayed at the end of the quiz
 	
-	Timer timer = new Timer(1000, new ActionListener() { 
+	Timer timer = new Timer(1000, new ActionListener() { //sets up the timer to decrement every 1000 milliseconds (one second)
 		
 		@Override
 		public void actionPerformed(ActionEvent e) { 
@@ -94,7 +95,7 @@ public class Quiz implements ActionListener {
 			seconds_left.setText(String.valueOf(seconds)); 
 			if(seconds <= 0) { 
 				displayCorrectResult(); 
-			}
+			} // decrements the timer until it hits zero in which it will call the displayCorrectResult method
 		}
 	});
 	
@@ -105,6 +106,7 @@ public class Quiz implements ActionListener {
 		frame.getContentPane().setBackground(new Color(0, 0, 0)); 
 		frame.setLayout(null); 
 		frame.setResizable(false); 
+		// sets up the size of the window, the colour, whether it is resizable and its size.
 		
 		textfield.setBounds(0, 0, 1000, 45); 
 		textfield.setBackground(new Color(25,25,25));
@@ -114,6 +116,7 @@ public class Quiz implements ActionListener {
 		textfield.setHorizontalAlignment(JTextField.CENTER); 
 		textfield.setEditable(false); 
 		textfield.setText(""); 
+		// sets up the title box, its colour, its size, its text, whether its editable and the alignment
 		
 		textarea.setBounds(0, 50, 1000, 90); 
 		textarea.setLineWrap(true); 
@@ -123,6 +126,7 @@ public class Quiz implements ActionListener {
 		textarea.setBorder(null);
 		textarea.setEditable(false);
 		textarea.setText("");
+		// sets up the question box with similar attributes to the title
 		
 		buttonA.setBounds(20, 160, 475, 100);
 		buttonA.setFont(new Font("Comic Sans", Font.PLAIN, 35)); 
@@ -132,6 +136,7 @@ public class Quiz implements ActionListener {
 		buttonA.setBackground(new Color(80, 80, 80));
 		buttonA.setForeground(new Color(255, 255, 255));
 		buttonA.setBorder(BorderFactory.createBevelBorder(1));
+		// sets the properties for the A button. Same as for buttonB, buttonC and buttonD
 		
 		buttonB.setBounds(490, 160, 475, 100);
 		buttonB.setFont(new Font("Comic Sans", Font.PLAIN, 35));
@@ -164,6 +169,7 @@ public class Quiz implements ActionListener {
 		answer_labelA.setBackground(new Color (50, 50, 50));
 		answer_labelA.setForeground(new Color(132, 216, 198)); 
 		answer_labelA.setFont(new Font("Comic Sans", Font.ITALIC, 35));
+		// creates the label next to buttonA for display of the text. Same as for answer_labelB, C and D
 		
 		answer_labelB.setBounds(490, 245, 500, 75); 
 		answer_labelB.setBackground(new Color (50, 50, 50)); 
@@ -188,6 +194,7 @@ public class Quiz implements ActionListener {
 		seconds_left.setOpaque(true);
 		seconds_left.setHorizontalAlignment(JTextField.CENTER); 
 		seconds_left.setText(String.valueOf(seconds));
+		// creates the seconds timer and its box for display
 		
 		time_label.setBounds(775, 540, 100, 25); 
 		time_label.setBackground(new Color(50, 50, 50));
@@ -195,6 +202,7 @@ public class Quiz implements ActionListener {
 		time_label.setFont(new Font("Comic Sans", Font.PLAIN, 28));
 		time_label.setHorizontalAlignment(JTextField.CENTER);
 		time_label.setText("Timer:");
+		// creates the red "Timer:" label next to the seconds_left box
 		
 		number_right.setBounds(290, 50, 200, 90); 
 		number_right.setBackground(new Color(25, 25, 25)); 
@@ -203,6 +211,7 @@ public class Quiz implements ActionListener {
 		number_right.setBorder(BorderFactory.createBevelBorder(1));
 		number_right.setHorizontalAlignment(JTextField.CENTER);
 		number_right.setEditable(false);
+		// creates the number_right box at the end of the quiz
 		
 		percentage.setBounds(490, 50, 200, 90);
 		percentage.setBackground(new Color(25,25,25));
@@ -211,6 +220,7 @@ public class Quiz implements ActionListener {
 		percentage.setBorder(BorderFactory.createBevelBorder(1));
 		percentage.setHorizontalAlignment(JTextField.CENTER);
 		percentage.setEditable(false);
+		// creates the percentage box at the end of the quiz for display next to the number_right box
 		
 		frame.setVisible(true); 
 		frame.add(time_label); 
@@ -225,13 +235,14 @@ public class Quiz implements ActionListener {
 		frame.add(buttonD); 
 		frame.add(textarea); 
 		frame.add(textfield); 
+		// adds everything defined above it to the window
 		
-		nextQuestion();
+		nextQuestion(); // calls the nextQuestion() method
 	}
-	public void nextQuestion() { 
+	public void nextQuestion() { // method responsible for generating the next question
 		
 		if (index >= total_questions) {
-			results(); 
+			results(); // if the index is equal to 15, the method of results() will be called displaying the number_right and percentage right
 		}
 		else {
 			textfield.setText("Question " + (index + 1)); 
@@ -241,22 +252,24 @@ public class Quiz implements ActionListener {
 			answer_labelC.setText(options[index][2]); 
 			answer_labelD.setText(options[index][3]); 
 			timer.start();
+			// modifies the "textarea" so it can display the next question
 		}
 		
 	}
-	@Override
+	@Override // interrupts the code to perform a specific action. In this case, to listen for a mouse click event
 	public void actionPerformed(ActionEvent e) { 
 		
 			buttonA.setEnabled(false);
 			buttonB.setEnabled(false);
 			buttonC.setEnabled(false);
 			buttonD.setEnabled(false);
+			// disables the buttons so you cannot click them twice once you've guessed an answer
 			
 			if(e.getSource() == buttonA) { 
 				answer = 'A'; 
 				if (answer == answers[index]) { 
 					correct_guesses++; 
-				}
+				} // if the answer is A, the amount of correct_guesses increments by one. As for B, C and D
 			}
 			
 			if(e.getSource() == buttonB) { 
@@ -280,19 +293,21 @@ public class Quiz implements ActionListener {
 				}
 			}
 
-			displayCorrectResult(); 
+			displayCorrectResult(); //calls the displayCorrectResult method
 	}
 	public void displayCorrectResult() { 
 		
-		timer.stop();
+		timer.stop(); // stops the timer once a button has been pressed
 		
 		buttonA.setEnabled(false); 
 		buttonB.setEnabled(false); 
 		buttonC.setEnabled(false); 
 		buttonD.setEnabled(false); 
+		// disables the buttons once again to display the correct results so you cannot change your answer once you've selected your answer
 		
 		if(answers[index] != 'A') 
 			answer_labelA.setForeground(new Color(255, 0, 0));
+		// flips the colour of the answer_lablelA from turquoise (its original colour) to red in the even that A is not the answer. As for B, C and D
 		
 		if(answers[index] != 'B') 
 			answer_labelB.setForeground(new Color(255, 0, 0)); 
@@ -303,24 +318,24 @@ public class Quiz implements ActionListener {
 		if(answers[index] != 'D') 
 			answer_labelD.setForeground(new Color(255, 0, 0));
 		
-		Timer pause = new Timer(2000, new ActionListener() { 
+		Timer pause = new Timer(2000, new ActionListener() { // creates a timer for flipping the colour back to turquoise in the event that colour was flipped to red. Changes the colour back after 2000 milliseconds or two seconds
 			
 			@Override
 			public void actionPerformed(ActionEvent e) { 
 				
-				answer_labelA.setForeground(new Color(132, 216, 198)); 
+				answer_labelA.setForeground(new Color(132, 216, 198)); // defines the colour that the timer needs to flip back to after a button was pressed. As for A, B, C and D
 				answer_labelB.setForeground(new Color(132, 216, 198)); 
 				answer_labelC.setForeground(new Color(132, 216, 198)); 
 				answer_labelD.setForeground(new Color(132, 216, 198)); 
 				
 				answer = ' '; 
 				seconds = 10; 
-				seconds_left.setText(String.valueOf(seconds)); 
+				seconds_left.setText(String.valueOf(seconds)); // converts the integer value of seconds_left from an integer to a string for display  
 				buttonA.setEnabled(true); 
 				buttonB.setEnabled(true); 
 				buttonC.setEnabled(true); 
 				buttonD.setEnabled(true); 
-				index++;
+				index++; // increments the index
 				nextQuestion();
 			}
 		});
@@ -328,12 +343,13 @@ public class Quiz implements ActionListener {
 		pause.start(); 
 		
 	}
-	public void results() { 
+	public void results() { // results method
 		
 		buttonA.setEnabled(false);
 		buttonB.setEnabled(false);
 		buttonC.setEnabled(false);
 		buttonD.setEnabled(false);
+		// disables the buttons once the quiz is over and shows the result
 		
 		results = (int)((correct_guesses / (double)total_questions) * 100);
 		
@@ -349,6 +365,7 @@ public class Quiz implements ActionListener {
 		
 		frame.add(percentage);
 		frame.add(number_right);
+		// adds the two functions to the window at the end
 		
 	}
 }
